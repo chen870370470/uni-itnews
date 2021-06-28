b3188faf64b348360083249b991f6f82b8c30567
chenjikang <870370470@qq.com>
2021-06-28 23:03:18 +0800

    关注页面

--- pages/tabbar/follow/follow.vue
+++ pages/tabbar/follow/follow.vue
@@ -1,6 +1,12 @@
 <template>
-	<view>
-		关注
+	<view class="follow">
+		<!-- 当行切换 -->
+		<view class="follow-tab">
+			<view class="follow-tab_box">
+				<view class="follow-tab__item" :class="{active:activeIndex === 0}" @click="tab(0)">文章</view>
+				<view class="follow-tab__item" :class="{active:activeIndex === 1}" @click="tab(1)">作者</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,15 +14,59 @@
 	export default {
 		data() {
 			return {
-				
+				activeIndex: 0
 			}
 		},
 		methods: {
-			
+			tab(index) {
+				this.activeIndex = index
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	page {
+		height: 100%;
+		display: flex;
+	}
 
+	.follow {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+		box-sizing: border-box;
+
+		.follow-tab {
+			height: 30px;
+			padding: 10px 20px;
+			border: 1px #F5F5F5 solid;
+
+			.follow-tab_box {
+				display: flex;
+				width: 100%;
+				height: 100%;
+				border-radius: 5px;
+				border: 1px $mk-base-color solid;
+
+				.follow-tab__item {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					width: 100%;
+					color: #666666;
+					font-size: 14px;
+
+					&:first-child {
+						border-right: 1px $mk-base-color solid;
+					}
+
+					&.active {
+						color: $mk-base-color;
+					}
+				}
+			}
+		}
+	}
 </style>
